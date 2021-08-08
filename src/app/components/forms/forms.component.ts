687a5fbf2fd6d6cf1faa56be4f3dcaa9c8edec1e
lch-howger <360000521@qq.com>
2021-08-08 18:07:52 +0800

    update service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class FormsComponent implements OnInit {
 
 
-  constructor() {
+  constructor(public storage:StorageService) {
   }
 
   ngOnInit(): void {
